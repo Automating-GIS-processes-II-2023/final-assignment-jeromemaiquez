286453761fa335393955ec6da81e41e05ec01934
jeromemaiquez <maiquez.jerome.ml@gmail.com>
2024-01-24 22:29:45 +0800

    Reimplemented plotting as a separate function

--- urban_indicators_scripts.py
+++ urban_indicators_scripts.py
@@ -6,6 +6,7 @@ List of included functions:
     - drop_nan_cols()
     - get_buffer_from_place()
     - parse_street_lanes()
+    - plot_streets()
 
 Usage:
     ./urban_indicators_scripts.py
@@ -153,6 +154,7 @@ def get_buffer_from_place(query, dist):
     # Reproject back to WGS84 (for OSMnx), then return buffered GDF
     return buffer.to_crs("EPSG:4326")
 
+
 def parse_street_lanes(streets_gdf):
     """
     Function to clean up and parse the `lanes` column
@@ -188,3 +190,114 @@ def parse_street_lanes(streets_gdf):
     streets_gdf["lanes"] = streets_gdf["lanes"].astype(int)
 
     return streets_gdf
+
+
+def plot_streets(streets_gdf, ring_gdf, width_factor=0.25, color_scheme="Greys_r"):
+    """
+    Function to plot street network GDF in a minimalist style.
+
+    Parameters
+    ----------
+    streets_gdf: <geopandas.geodataframe.GeoDataFrame>
+        Street network GeoDataFrame to be plotted.
+    ring_gdf: <geopandas.geodataframe.GeoDataFrame>
+        Buffer ring GeoDataFrame, whose bounds will be the basis for the axes.
+    width_factor: <float>
+        Scaling factor between number of lanes and line width in plot.
+    color_scheme: <str> or <tuple>
+        Matplotlib color map to use for edge and face colors.
+
+    """
+    
+    # Import packages
+    import geopandas as gpd
+    import matplotlib.pyplot as plt
+    from matplotlib import colormaps as cmaps
+
+    # Assertions to weed out incorrect data types
+    assert type(streets_gdf) == gpd.geodataframe.GeoDataFrame, "streets gdf must be geodataframe"
+    assert all(x in streets_gdf.columns for x in ["osmid", "lanes", "geometry"]), "streets gdf must be from OSM"
+    assert type(ring_gdf) == gpd.geodataframe.GeoDataFrame, "buffer gdf must be geodataframe"
+    assert all(x in ring_gdf.columns for x in ["name", "geometry"]), "buffer gdf must have name & geometry"
+    
+    assert type(width_factor) == float, "width factor must be a float"
+    assert 0.0 <= width_factor <= 1.0, "width factor must be between 0 and 1"
+    assert type(color_scheme) in (str, tuple), "color_scheme must follow matplotlib color formats"
+    
+    # Get min and max number of lanes
+    min_lanes = streets_gdf["lanes"].min()
+    max_lanes = streets_gdf["lanes"].max()
+
+    # Assign line width per number of lanes
+    linewidths = {lane: lane * width_factor for lane in range(min_lanes, max_lanes + 1)}
+
+    # Create figure and axis
+    fig, ax = plt.subplots(figsize=(10, 10))
+
+    # Assign edge & face colors from specified color scheme
+    cmap = cmaps[color_scheme].resampled(5)
+    edgecolor = cmap(1.0)
+    facecolor = cmap(0.0)
+    ringcolor = cmap(0.8)
+
+    # For each lane number in streets gdf...
+    for lane, data in streets_gdf.groupby("lanes"):
+        # Plot all streets with x no. of lanes...
+        data.plot(
+            color=edgecolor,
+            ax=ax,
+            label=lane,
+            linewidth=linewidths[lane] # ...with the correct line width
+        )
+    
+    # Get bounding box of buffer ring...
+    proj_ring_gdf = ring_gdf.to_crs(streets_gdf.crs)
+    minx, miny, maxx, maxy = proj_ring_gdf.total_bounds
+
+    # ...to set x- and y-limits of axis (to ensure it is square)
+    ax.set_xlim(left=minx - 200, right=maxx + 200)
+    ax.set_ylim(bottom=miny - 200, top=maxy + 200)
+
+    # Set other axis properties
+    ax.set_facecolor(facecolor)
+    ax.set_title(
+        ring_gdf.loc[0, "name"],
+        loc="left",
+        color=edgecolor,
+
+        fontsize=45.0,
+        fontfamily="Garamond",
+        fontstretch="expanded",
+
+        y=0.08, # 8% of figure height (from bottom)
+        x=0.03, # 3% of figure width (from left)
+        pad=3.0
+    )
+    ax.text(
+        x=minx-50,
+        y=miny-50,
+        s="Road Network",
+        color=edgecolor,
+        fontsize=20.0,
+        fontfamily="Garamond",
+        fontstretch="expanded"
+    )
+
+    # Add buffer ring to plot
+    proj_ring_gdf.plot(
+        edgecolor=ringcolor,
+        facecolor="none",
+        linewidth=0.75,
+        linestyle="dotted",
+        ax=ax
+    )
+
+    # Set colors of figure
+    fig.set_facecolor(facecolor)
+    fig.set_edgecolor(facecolor)
+
+    # Remove axis labels for photo finish
+    ax.set_axis_off()
+
+    # Show plot
+    plt.show()
